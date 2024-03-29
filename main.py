639e4e0d78550ef7c414075e6bd3f63cfd931464
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-03-29 12:43:51 +0100

    new4

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-with open("text_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("text_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("text_file.txt", mode="w") as file:
+    file.write("1")
